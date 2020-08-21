d8ee1f939129a75aeba262bc7e960e09856a49de
Dmitrii <deigumnov@gmail.com>
2020-08-21 23:52:00 +1000

    Добавлены комментарии

--- src/main/java/com/company/Super.java
+++ src/main/java/com/company/Super.java
@@ -1,19 +1,24 @@
 package com.company;
 
+import org.graalvm.compiler.api.replacements.Snippet;
+
 import java.util.LinkedList;
 
 public class Super {
-// позиция объекта
+// координаты объекта X, Y, Z
     private double posx, posy, posz;
-// тип объекта
+// тип объекта (порода)
     private String type;
+// воспроизводимые звуки
     private LinkedList<String> message = new LinkedList<>();
+
     public void setXYZ(double posx, double posy, double posz) {
         this.posx = posx;
         this.posy = posy;
         this.posz = posz;
     }
 
+// немного уменьшает количество кода в основной программе
     public String getXYZ () {
         String pos = "x = " + posx;
         pos += " y = " + posy;
@@ -33,19 +38,24 @@ public class Super {
         return posz;
     }
 
+// устанавливает тип объекта (порода)
     public void setType (String type) { this.type = type; }
 
+// возвращает тип объекта (порода)
     public String getType () {return this.type; }
 
+// добавляет воспроизводимые звуки
     public void setMessage (String ... message) {
         int thisMessageSize = this.message.size();
         for (int i = thisMessageSize;i < message.length+thisMessageSize; ++i) this.message.add(message[i]);
     }
 
+// возвращает количество воспроизводимых сообщений
     public int getMessageCount () {
         return this.message.size();
     }
 
+// возвращает воспроизводимые звуки по номеру
     public String getMessage (int messageNumber) {
         if ((messageNumber >= 0) && (messageNumber < this.message.size())) {
             return message.get(messageNumber);
