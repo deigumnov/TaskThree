bf5f4ee5e0d84f6a6e62cc4ab10c7dfe62feef9c
Dmitrii <deigumnov@gmail.com>
2020-08-30 22:20:54 +1000

    Animals

---
+++ src/main/java/com/company/Dog.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Dog extends Super {
+    public Dog(String type) {
+        super(type);
+        setMessage("ГАВ");
+    }
+}
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,8 +1,52 @@
 package com.company;
 
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
+        LinkedList<Super> animals = new LinkedList<>();
 
+        animals.add(new Cat("Чеширский кот"));
+        animals.add(new Dog("Овчарка"));
+
+        for (Super animal : animals) {
+            // все научились рычать
+            animal.setMessage("РРР");
+            if (animal instanceof Cat) {
+                // коты ушли на север
+                animal.setXYZ(0, 999, 0);
+            } else if (animal instanceof Dog) {
+                // собаки ушли на восток
+                animal.setXYZ(999, 0, 0);
+            } else {
+                // неописанный класс улетел
+                animal.setXYZ(0, 0, 999);
+            }
+            for (int j = 0; j < animal.getMessageCount(); j++) {
+                if (animal instanceof Cat) {
+                    System.out.println("Кот говорит: " + animal.getMessage(j));
+                } else if (animal instanceof Dog) {
+                    System.out.println("Пес говорит: " + animal.getMessage(j));
+                } else {
+                    System.out.println("НЛО говорит: " + animal.getMessage(j));
+                }
+            }
+        }
+
+        Cat catSimple = new Cat( "Чеширский кот");
+        Dog dogSimple = new Dog("Овчарка");
+
+        catSimple.setXYZ(4,5,0);
+        dogSimple.setXYZ(1,2,0);
+
+        System.out.println(catSimple.getType() + catSimple.getXYZ());
+        System.out.println(dogSimple.getType() + dogSimple.getXYZ());
+
+        System.out.println("Кот говорит: " + catSimple.getMessage(0));
+        System.out.println("Пес говорит: " + dogSimple.getMessage(0));
+
+        System.out.println("Кот " + catSimple.jumpOnTree(5));
+        System.out.println("Кот " + catSimple.jumpFromTree());
     }
 }
